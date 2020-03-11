888456e6f33102460de3d6372bbe6a6ff9c43b86
tristan49105 <tristan.verschuere@gmail.com>
2020-03-11 09:04:50 +0100

    First commi

---
+++ Accueil.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Commune de La Forestière</title>
+</head>
+<body>
+
+Bienvenue sur le site internet de la commune de La Forestière
+    
+</body>
+</html>
