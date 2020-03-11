d72765c10118f3f5a26cbf21da34b4b101de7002
tristan49105 <tristan.verschuere@gmail.com>
2020-03-11 14:36:02 +0100

    Creation Bandeau
    Début création bandeau. Attente Responsive

--- Accueil.php
+++ Accueil.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Commune de La Forestière</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="police/police.css" />
 </head>
 <body>
 
-Bienvenue sur le site internet de la commune de La Forestière
+<?php include("bandeau.php") ?>
     
 </body>
 </html>
