ef923e3be0c3ef89179b728d3956c50cfd0a0bca
tristan49105 <tristan.verschuere@gmail.com>
2020-03-12 11:02:05 +0100

    Bandeau Responsive

--- bandeau.php
+++ bandeau.php
@@ -1,14 +1,14 @@
 <div id="bandeau">
 
-<div id="logo"></div>
+   <div id="logo"><img src="Ressources/logo.png" alt="Logo La Forestière" title="Logo La Forestière" width=125px
+    height=125px/></div>
 
-<div id="navigation">
-   <ul>
-      <li><a style="text-decoration:none" href="Accueil.php">Accueil</a></li>
-      <li><a style="text-decoration:none" href="Le village.php">Le Village</a></li>
-      <li><a style="text-decoration:none" href="Mairie.php">Mairie</a></li>
-      <li><a style="text-decoration:none" href="Vos Démarches.php">Vos Démarches</a></li>
-    </ul>
-</div>
-
+      <div id="navigation">
+         <ul>
+            <li><a style="text-decoration:none" href="Accueil.php">Accueil</a></li>
+            <li><a style="text-decoration:none" href="Le village.php">Le Village</a></li>
+            <li><a style="text-decoration:none" href="Mairie.php">Mairie</a></li>
+            <li><a style="text-decoration:none" href="Vos Démarches.php">Vos Démarches</a></li>
+         </ul>
+      </div>
 </div>
